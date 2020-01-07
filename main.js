036300e8dbc61ae11390ffa47a1903f1ab07dccf
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-07 08:55:18 -0600

    Setup basic js to pull values from form elements and set styles accordingly.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('main.js loaded');
+const bgColorInput = document.querySelector('#bg-color');
+const fontInput = document.querySelector('#font');
+const imgInput = document.querySelector('#img');
+
+const img = document.querySelector('img');
+const p = document.querySelector('p');
+
+const htmlElement = document.querySelector('html');
+
+htmlElement.setAttribute('style', `background-color: #${bgColorInput.value};`);
+img.src = imgInput.value;
+p.setAttribute('style', `font-family: ${fontInput.value};`);
+
