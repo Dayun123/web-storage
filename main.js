f381c2445b287de2c3c9eecff85970c5af8f95dc
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-01-07 09:07:01 -0600

    Set page styles to change on form changes.

--- main.js
+++ main.js
@@ -11,7 +11,15 @@ htmlElement.setAttribute('style', `background-color: #${bgColorInput.value};`);
 img.src = imgInput.value;
 p.setAttribute('style', `font-family: ${fontInput.value};`);
 
-document.addEventListener('change', function(e) {
-  console.log(e.target);
+bgColorInput.addEventListener('change', function() {
+  htmlElement.setAttribute('style', `background-color: #${this.value};`);
+});
+
+fontInput.addEventListener('change', function() {
+  p.setAttribute('style', `font-family: ${this.value};`);
+});
+
+imgInput.addEventListener('change', function() {
+  img.src = this.value;
 });
 
